e01c114893c9c20290b0e739d0682fa73d548ffa
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-09-05 23:15:06 +0800

    Initial Commit

---
+++ ilimi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ilimi
+//
+//  Created by 陳奕利 on 2022/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
