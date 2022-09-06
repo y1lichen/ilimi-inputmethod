32bc9deb0b05a86b849f3168385b21b2b017bdb3
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-09-07 00:24:06 +0800

    PhraseInitializer added

--- ilimi/Utils/PhraseInitilizer.swift
+++ ilimi/Utils/PhraseInitilizer.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import CoreData
+
+class PhraseInitilizer {
+	
+	let persistenceContainer = PersistenceController.shared
+	let path = NSHomeDirectory() + "/liu.json"
+	
+	init() {
+		initPhraseData()
+	}
+	
+	func initPhraseData() {
+		do {
+			let data = try Data(contentsOf: URL(fileURLWithPath: path))
+			if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
+				if let chardefs = json["chardefs"] as? [String: [String]] {
+					for (key, value) in chardefs {
+						let model = NSEntityDescription.insertNewObject(forEntityName: "Phrase", into: persistenceContainer.container.viewContext) as! Phrase
+						model.key = key
+						model.value = value as NSObject
+					}
+					persistenceContainer.saveContext()
+				}
+			}
+		} catch {
+			NSLog(String(describing: error))
+		}
+	}
+}
