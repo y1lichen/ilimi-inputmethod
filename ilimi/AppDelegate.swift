20261f9b92bfec22e782ca24a745f9f333d67b48
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-09-08 23:44:54 +0800

    AppDelegate

--- ilimi/AppDelegate.swift
+++ ilimi/AppDelegate.swift
@@ -4,5 +4,41 @@
 //
 //  Created by 陳奕利 on 2022/9/5.
 //
+// AppDelegate.swift
 
-import Foundation
+import Cocoa
+import InputMethodKit
+
+// necessary to launch this app
+class NSManualApplication: NSApplication {
+	private let appDelegate = AppDelegate()
+
+	override init() {
+		super.init()
+		self.delegate = appDelegate
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+	var server = IMKServer()
+	var candidatesWindow = IMKCandidates()
+	var phraseInitializer: PhraseInitilizer? = nil
+
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		// Insert code here to initialize your application
+		server = IMKServer(name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String, bundleIdentifier: Bundle.main.bundleIdentifier)
+		candidatesWindow = IMKCandidates(server: server, panelType: kIMKSingleRowSteppingCandidatePanel, styleType: kIMKMain)
+		phraseInitializer =  PhraseInitilizer()
+		NSLog("tried connection")
+	}
+
+	func applicationWillTerminate(_ notification: Notification) {
+		// Insert code here to tear down your application
+	}
+	
+}
