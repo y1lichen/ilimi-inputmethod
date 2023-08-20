aed53f225b7a7aca139a3f3204e5c898ff7e254a
Jefferson Chen <f1jefferson@yahoo.com.tw>
2023-08-20 12:14:59 +0800

    setting view for adjusting candidates panel

--- ilimi/View/Settings/GeneralSettingsView.swift
+++ ilimi/View/Settings/GeneralSettingsView.swift
@@ -9,16 +9,23 @@ import Foundation
 import SwiftUI
 
 struct GeneralSettingsView: View {
-    @AppStorage("fontSize") private var fontSize = 24
+    // 預設字體大小 22
+    // 預設橫排選字窗
+    @AppStorage("fontSize") private var fontSize = 22
+    @AppStorage("isHorizontalCandidatesPanel") private var isHorizontalCandidatesPanel = true
     let fontSizeValues = [14, 16, 18, 20, 22, 24, 28, 32]
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 20) {
             Picker("字體大小", selection: $fontSize) {
                 fontPickerContent()
             }.onChange(of: fontSize) { _ in
                 NSApp.terminate(self)
             }
+            Picker("選字窗排列", selection: $isHorizontalCandidatesPanel) {
+                Text("橫式").tag(true)
+                Text("直式").tag(false)
+            }.pickerStyle(RadioGroupPickerStyle())
         }.frame(width: 250)
     }
     
