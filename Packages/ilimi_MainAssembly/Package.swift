8e4b2b8d7a65955f1321117446b1d306bd6b22e8
ShikiSuen <shikisuen@outlook.com>
2024-03-21 00:43:03 +0800

    Xcode // Unify minimum OS version to macOS 13.

--- Packages/ilimi_MainAssembly/Package.swift
+++ Packages/ilimi_MainAssembly/Package.swift
@@ -1,12 +1,11 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "ilimiMainAssembly",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
--- Packages/vChewing_IMKUtils_IlimiImpl/Package.swift
+++ Packages/vChewing_IMKUtils_IlimiImpl/Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
     name: "vChewing_IMKUtils_IlimiImpl",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v13),
     ],
     products: [
         .library(
