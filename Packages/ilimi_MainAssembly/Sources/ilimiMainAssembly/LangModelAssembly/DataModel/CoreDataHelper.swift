111c792fc603c90f701e96a158674928c766f92c
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-03-23 14:07:27 +0800

    // 修正同音輸入

--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/LangModelAssembly/DataModel/CoreDataHelper.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/LangModelAssembly/DataModel/CoreDataHelper.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by 陳奕利 on 2024/3/23.
 //
@@ -9,6 +9,21 @@ import Foundation
 import CoreData
 
 class CoreDataHelper {
+	// 取得文字的注音碼的raw value（英文碼）
+	static func getRawZhuyinOfChar(_ text: String) -> [String] {
+		let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
+		request.predicate = NSPredicate(format: "value == %@", text)
+		do {
+			let response = try PersistenceController.shared.container.viewContext.fetch(request)
+			let res = response.compactMap { $0.key }
+			return res
+		} catch {
+			NSLog(error.localizedDescription)
+		}
+		return []
+	}
+	
+	// 查找文字的注音碼
 	static func getZhuyinOfChar(_ char: String) -> [String] {
 		let requestForZhuyin = NSFetchRequest<Zhuin>(entityName: "Zhuin")
 		requestForZhuyin.predicate = NSPredicate(format: "value == %@", char)
@@ -25,6 +40,7 @@ class CoreDataHelper {
 		return res
 	}
 	
+	// 查找文字的嘸蝦米輸入碼
 	static func getKeyOfChar(_ char: String) -> [String] {
 		let requestForKey = NSFetchRequest<Phrase>(entityName: "Phrase")
 		requestForKey.predicate = NSPredicate(format: "value == %@", char)
@@ -39,5 +55,26 @@ class CoreDataHelper {
 			NSLog(error.localizedDescription)
 		}
 		return res
+	}
+	
+	// 取得相同讀音的候選字
+	static func getCharWithSamePronunciation(_ char: String) -> [String] {
+		let zhuyins: [String] = getRawZhuyinOfChar(char)
+		var result: [String] = []
+		let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
+		do {
+			for zhuyin in zhuyins {
+				request.predicate = NSPredicate(format: "key == %@", zhuyin)
+				let response = try PersistenceController.shared.container.viewContext.fetch(request)
+				for item in response {
+					if let itemValue = item.value, itemValue != char {
+						result.append(itemValue)
+					}
+				}
+			}
+		} catch {
+			NSLog(error.localizedDescription)
+		}
+		return result
 	}
 }
--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/Utils/InputEngine.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/Utils/InputEngine.swift
@@ -7,90 +7,62 @@ import CoreData
 import Foundation
 
 struct InputEngine {
-    static let shared = InputEngine()
-
-    // 取得以注音輸入的候選字
-    func getCadidatesByZhuyin(_ text: String) {
-        let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
-        request.predicate = NSPredicate(format: "key BEGINSWITH %@", text)
-        request.sortDescriptors = [
-            NSSortDescriptor(key: "key.length", ascending: true),
-            NSSortDescriptor(key: "key_priority", ascending: true),
-        ]
-        do {
-            let response = try PersistenceController.shared.container.viewContext.fetch(request)
-            let candidates: [String] = response.compactMap { $0.value }
-            InputContext.shared.candidates = candidates
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-    }
-
-    // 取的以嘸蝦米輸入的候選字
-    func getCandidates(_ text: String) {
+	static let shared = InputEngine()
+	
+	// 取得以注音輸入的候選字
+	func getCadidatesByZhuyin(_ text: String) {
+		let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
+		request.predicate = NSPredicate(format: "key BEGINSWITH %@", text)
+		request.sortDescriptors = [
+			NSSortDescriptor(key: "key.length", ascending: true),
+			NSSortDescriptor(key: "key_priority", ascending: true),
+		]
+		do {
+			let response = try PersistenceController.shared.container.viewContext.fetch(request)
+			let candidates: [String] = response.compactMap { $0.value }
+			InputContext.shared.candidates = candidates
+		} catch {
+			NSLog(error.localizedDescription)
+		}
+	}
+	
+	// 取的以嘸蝦米輸入的候選字
+	func getCandidates(_ text: String) {
 		// 輸入碼太長的話就不用查詢，節省資源
 		if text.count >= 6 {
 			InputContext.shared.preInputPrefixSet = []
 			InputContext.shared.candidates = []
 		}
-        let request = NSFetchRequest<Phrase>(entityName: "Phrase")
-        request.predicate = NSPredicate(format: "key BEGINSWITH %@", text)
-        request.sortDescriptors = [NSSortDescriptor(key: "key_priority", ascending: true)]
-        do {
-            let response = try PersistenceController.shared.container.viewContext.fetch(request)
-            var candidates: [String] = []
-            var candidatesSet: Set<String> = []
-            var inputStrSet: Set<String> = []
-            for r in response {
-                let value: String = r.value(forKey: "value") as! String
-                if let rKey = r.key, rKey.count > text.count {
-                    inputStrSet.insert(String(rKey.prefix(text.count + 1)))
-                }
-                if candidatesSet.contains(value) {
-                    continue
-                }
-                candidatesSet.insert(value)
-                candidates.append(value)
-            }
-            InputContext.shared.preInputPrefixSet = inputStrSet
-            InputContext.shared.candidates = candidates
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-    }
-
-    // 取得相同讀音的候選字
-    func getCandidatesByPronunciation(_ text: String) {
-        let zhuyins: [String] = getKeysOfChar(text)
-        let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
-        var result: [String] = []
-        do {
-            for zhuyin in zhuyins {
-                request.predicate = NSPredicate(format: "key == %@", zhuyin)
-                let response = try PersistenceController.shared.container.viewContext.fetch(request)
-                for item in response {
-                    if let itemValue = item.value, itemValue != text {
-                        result.append(itemValue)
-                    }
-                }
-            }
-            InputContext.shared.candidates = result
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-    }
-
-    // 取得文字的注音碼
-    func getKeysOfChar(_ text: String) -> [String] {
-        let request = NSFetchRequest<Zhuin>(entityName: "Zhuin")
-        request.predicate = NSPredicate(format: "value == %@", text)
-        do {
-            let response = try PersistenceController.shared.container.viewContext.fetch(request)
-            let res = response.compactMap { $0.key }
-            return res
-        } catch {
-            NSLog(error.localizedDescription)
-        }
-        return []
-    }
+		let request = NSFetchRequest<Phrase>(entityName: "Phrase")
+		request.predicate = NSPredicate(format: "key BEGINSWITH %@", text)
+		request.sortDescriptors = [NSSortDescriptor(key: "key_priority", ascending: true)]
+		do {
+			let response = try PersistenceController.shared.container.viewContext.fetch(request)
+			var candidates: [String] = []
+			var candidatesSet: Set<String> = []
+			var inputStrSet: Set<String> = []
+			for r in response {
+				let value: String = r.value(forKey: "value") as! String
+				if let rKey = r.key, rKey.count > text.count {
+					inputStrSet.insert(String(rKey.prefix(text.count + 1)))
+				}
+				if candidatesSet.contains(value) {
+					continue
+				}
+				candidatesSet.insert(value)
+				candidates.append(value)
+			}
+			InputContext.shared.preInputPrefixSet = inputStrSet
+			InputContext.shared.candidates = candidates
+		} catch {
+			NSLog(error.localizedDescription)
+		}
+	}
+	
+	// 取得相同讀音的候選字
+	func getCandidatesByPronunciation(_ text: String) {
+		InputContext.shared.candidates = CoreDataHelper.getCharWithSamePronunciation(text)
+	}
+	
+	
 }
