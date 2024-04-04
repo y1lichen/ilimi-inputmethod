0009109481a802b3054d0a9dc52e9b456ea9d4c1
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-04-04 20:23:38 +0800

    // EditableText add editable textfield

--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/View/AddCustomPhrase/EditableText.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/View/AddCustomPhrase/EditableText.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct EditableText: View {
+	@Binding var text: String
+
+	@State private var temporaryText: String
+	@FocusState private var isFocused: Bool
+
+	init(text: Binding<String>) {
+		self._text = text
+		self.temporaryText = text.wrappedValue
+	}
+
+	var body: some View {
+		TextField("", text: $temporaryText, onCommit: { text = temporaryText })
+			.focused($isFocused, equals: true)
+			.onTapGesture { isFocused = true }
+			.onExitCommand { temporaryText = text; isFocused = false }
+	}
+}
