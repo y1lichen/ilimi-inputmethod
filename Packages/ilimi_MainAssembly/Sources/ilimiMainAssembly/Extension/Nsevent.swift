dc5f7ec8481317845ed3c34b74cd8740ac14f3ff
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-04-24 18:39:51 +0800

    update README && fix missing commit

---
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/Extension/Nsevent.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by 陳奕利 on 2024/4/18.
+//
+
+import Foundation
+import AppKit
+
+extension NSEvent {
+	var isDeleteKey: Bool {
+		keyCode == 51
+	}
+}
---
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/View/AddCustomPhrase/AddCustomPhraseSheetView.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  
+//
+//  Created by 陳奕利 on 2024/4/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct SheetView: View {
+	
+	@ObservedObject var viewModel: CustomPhraseViewModel
+
+	init(viewModel: CustomPhraseViewModel) {
+		self.viewModel = viewModel
+	}
+
+	var body: some View {
+		VStack {
+			Text("新增自訂字詞")
+				.font(.headline)
+				.fontWeight(.heavy)
+			Spacer()
+			HStack {
+				TextField("字碼", text: $viewModel.key)
+					.frame(width: 80)
+				TextField("字詞", text: $viewModel.value)
+			}
+			Spacer().frame(maxHeight: 20)
+			HStack {
+				Button("取消") {
+					viewModel.showAddSheet = false
+				}
+				Spacer()
+				Button("完成") {
+					if !viewModel.checkIsValid() {
+						return
+					}
+					if viewModel.showAddSheet {
+						viewModel.addCustomPhrase()
+						viewModel.showAddSheet = false
+					} else if viewModel.showEditSheet {
+						viewModel.editCustomPhrase()
+						viewModel.showEditSheet = false
+					}
+				}
+			}
+		}
+		.frame(width: 300, height: 100)
+		.padding()
+		.onAppear {
+				viewModel.syncKeyValueWithCustomPhraseToBeEdited()
+		}
+	}
+}
