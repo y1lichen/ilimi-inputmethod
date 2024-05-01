c8339a8ac44c7d4406c457b571390641add5976c
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-05-01 19:12:09 +0800

    formatting

--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/LangModelAssembly/WordDictReader/LiuUniTabConverter.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/LangModelAssembly/WordDictReader/LiuUniTabConverter.swift
@@ -22,28 +22,28 @@ class LiuUniTabConverter {
         }
     }
 
-	func getint16(addr: Int) -> Int {
-		return Int(bytes[addr]) | Int(bytes[addr + 1]) << 8
-	}
+    func getint16(addr: Int) -> Int {
+        return Int(bytes[addr]) | Int(bytes[addr + 1]) << 8
+    }
 
-	func getbits(_ start: Int, _ nbit: Int, _ i: Int) -> Int {
-		if nbit == 1 || nbit == 2 || nbit == 4 {
-			let byte = bytes[start + i * nbit / 8]
-			let ovalue = Int(byte) >> (8 - nbit - i * nbit % 8)
-			return ovalue & ((1 << nbit) - 1)
-		} else if nbit > 0 && nbit % 8 == 0 {
-			let nbyte = nbit / 8
-			var value: Int = 0
-			var a = start + i * nbyte
-			for _ in 0..<nbyte {
-				value = value << 8 | Int(bytes[a])
-				a += 1
-			}
-			return value
-		} else {
-			fatalError("Invalid nbit")
-		}
-	}
+    func getbits(_ start: Int, _ nbit: Int, _ i: Int) -> Int {
+        if nbit == 1 || nbit == 2 || nbit == 4 {
+            let byte = bytes[start + i * nbit / 8]
+            let ovalue = Int(byte) >> (8 - nbit - i * nbit % 8)
+            return ovalue & ((1 << nbit) - 1)
+        } else if nbit > 0 && nbit % 8 == 0 {
+            let nbyte = nbit / 8
+            var value: Int = 0
+            var a = start + i * nbyte
+            for _ in 0 ..< nbyte {
+                value = value << 8 | Int(bytes[a])
+                a += 1
+            }
+            return value
+        } else {
+            fatalError("Invalid nbit")
+        }
+    }
 
     func utf8_chr(ord: Int) -> String {
         return String(UnicodeScalar(ord)!)
@@ -61,28 +61,28 @@ class LiuUniTabConverter {
         let i3 = i2 + getint16(addr: 6) // or + (words*1+7)/8
         let i4 = i3 + getint16(addr: 6) // or + (words*1+7)/8
 
-		let rootkey: [Character] = Array(" abcdefghijklmnopqrstuvwxyz,.'[]")
+        let rootkey: [Character] = Array(" abcdefghijklmnopqrstuvwxyz,.'[]")
 
         var count = 0
         for i in 0 ..< 1024 {
-			var key: [Character] = [Character](repeating: Character(" "), count: 4)
-			
-            key[0] = (rootkey[i / 32])
-            key[1] = (rootkey[i % 32])
+            var key: [Character] = [Character](repeating: Character(" "), count: 4)
+
+            key[0] = rootkey[i / 32]
+            key[1] = rootkey[i % 32]
 
             if key[0] == " " { continue }
 
             for ci in getint16(addr: i * 2) ..< getint16(addr: i * 2 + 2) {
-				let bit24 = getbits(i4, 24, ci)
-				let hi = getbits(i1, 2, ci)
-                let lo = bit24 & 0x3fff
-				key[2] = rootkey[bit24 >> 19]
-				key[3] = rootkey[bit24 >> 14 & 0x1f]
+                let bit24 = getbits(i4, 24, ci)
+                let hi = getbits(i1, 2, ci)
+                let lo = bit24 & 0x3FFF
+                key[2] = rootkey[bit24 >> 19]
+                key[3] = rootkey[bit24 >> 14 & 0x1F]
 
-				let keyString = String(key).trimmingCharacters(in: .whitespacesAndNewlines)
-				let chr = utf8_chr(ord: hi << 14 | lo)
+                let keyString = String(key).trimmingCharacters(in: .whitespacesAndNewlines)
+                let chr = utf8_chr(ord: hi << 14 | lo)
 
-				let flag_sp = getbits(i3, 1, ci)
+                let flag_sp = getbits(i3, 1, ci)
                 writeData(keyString, chr, Int64(count), flag_sp == 1 ? true : false)
 //                let flag_unknown = getbits(start: Int(i2), nbit: 1, i: Int(ci))
                 count += 1
