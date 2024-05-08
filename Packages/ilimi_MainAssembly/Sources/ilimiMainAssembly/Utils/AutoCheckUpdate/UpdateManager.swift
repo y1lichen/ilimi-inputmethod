d4df057f4dd499e31e192bb90d4c26484445fa9f
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-05-08 10:48:14 +0800

    // UpdateManager: compare version #18

--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/Utils/AutoCheckUpdate/UpdateManager.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/Utils/AutoCheckUpdate/UpdateManager.swift
@@ -12,7 +12,7 @@ import Foundation
 struct GitHubRelease: Codable {
     let tagName: String
     let name: String
-	let id: Int
+    let id: Int
 }
 
 // MARK: - UpdateManager
@@ -20,7 +20,7 @@ struct GitHubRelease: Codable {
 class UpdateManager {
     static let url = URL(string: "https://api.github.com/repos/y1lichen/ilimi-inputmethod/releases/latest")
 
-    static func getURLData(completion: @escaping (Result<GitHubRelease, Error>) -> ()) {
+    static func getURLData(completion: @escaping (Result<GitHubRelease, Error>) -> Void) {
         let session = URLSession.shared
 
         // 創建 URLSessionDataTask
@@ -70,23 +70,55 @@ class UpdateManager {
         // 開始任務
         task.resume()
     }
-	
-	static func showPopUp(_ appVer: String) {
-		
-	}
 
-    static func checkUpdate() {
+    // https://leetcode.com/problems/compare-version-numbers/description/
+    // 參考leetcode吧
+    private static func compareVersion(_ version1: String, _ version2: String) -> Int {
+        let v1Components = version1.components(separatedBy: ".")
+        let v2Components = version2.components(separatedBy: ".")
+
+        var i = 0
+
+        while i < max(v1Components.count, v2Components.count) {
+            let v1 = i < v1Components.count ? Int(v1Components[i]) ?? 0 : 0
+            let v2 = i < v2Components.count ? Int(v2Components[i]) ?? 0 : 0
+
+            if v1 < v2 {
+                return -1
+            } else if v1 > v2 {
+                return 1
+            }
+            i += 1
+        }
+        return 0
+    }
+
+	private static func showPopUp(_ appVer: String, _ remoteVer: String, _ isManual: Bool) {
+        var res = compareVersion(appVer, remoteVer)
+		if !isManual && res == 0 {
+			return
+		}
+		var displayText = "你己經擁有最新版本\(appVer)"
+		if res == 1 {
+			displayText = "你擁有的是測試版本\(appVer)。目前發佈版本為\(remoteVer)。"
+		} else if res == -1 {
+			displayText = "最新版本為\(remoteVer)，你擁有的是測試版本\(appVer)。前往更新吧！"
+		}
+
+    }
+
+	static func checkUpdate(_ isManual: Bool = false) {
         var appVer: String = ""
         if let infoDict = Bundle.main.infoDictionary {
             if !infoDict.isEmpty {
-				appVer = infoDict["CFBundleShortVersionString"] as! String? ?? "unkown"
+                appVer = infoDict["CFBundleShortVersionString"] as! String? ?? "unkown"
             }
         }
         getURLData { result in
             switch result {
             case let .success(data):
                 DispatchQueue.main.async {
-                    showPopUp(appVer)
+                    showPopUp(appVer, data.tagName, isManual)
                 }
 
             case let .failure(error):
