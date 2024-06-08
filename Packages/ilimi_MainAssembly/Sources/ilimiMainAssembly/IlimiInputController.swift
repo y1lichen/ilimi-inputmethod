08ea60bbe551eb771267d894eb6a4dd437f0432a
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-06-08 22:39:05 +0800

    快打模式下如果沒有候選字時，可以直接輸出英數 #23

--- Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/IlimiInputController.swift
+++ Packages/ilimi_MainAssembly/Sources/ilimiMainAssembly/IlimiInputController.swift
@@ -247,7 +247,12 @@ extension IlimiInputController {
 
     func commitText(client sender: Any!, text: String) {
         // 在快打模式下如果不是最簡碼不會輸出，並且會要求使用重新最簡碼重打
-        if !isASCIIMode, InputContext.shared.isSpMode, !SpModeManager.checkInputIsSp(text, assistSelectChar.chr) {
+		// 快打模式下如果沒有候選字時，可以直接輸出英數
+        if !isASCIIMode, InputContext.shared.isSpMode, !InputContext.shared.candidates.isEmpty,
+           !SpModeManager.checkInputIsSp(
+               text,
+               assistSelectChar.chr
+           ) {
             cleanComposition()
             let res = SpModeManager.getSpKeyOfChar(text).joined(separator: "、")
             NotifierController.notify(message: "\(text)的簡碼為\(res)", stay: true)
